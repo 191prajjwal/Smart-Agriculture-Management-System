d791a2a3bbebb3040622849f0596336b72849b37
191prajjwal <191sprajjwal@gmail.com>
2025-01-11 11:51:06 +0530

    created auth model route and middleware

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require("express")
+const mongoose = require ("mongoose")
+const cors= require("cors")
+const dotenv= require("dotenv")
+const authRoutes= require("./routes/authRoutes")
+dotenv.config()
+
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use("/api/auth",authRoutes)
+
+
+
+app.get("/",(req,res)=>{
+    res.send("API is Working")
+})
+
+
+mongoose.connect(`${process.env.MONGODB_URL}`).then(()=>{
+    console.log("Database connected")
+}).catch((err)=>{
+    console.error('Database connection error',err)
+})
+
+const PORT= process.env.PORT || 5000
+app.listen(PORT,()=>{
+    console.log("server is running on PORT "+ PORT)
+})
